3a151a9150510587ca2f8ef3258c39bcd451d9c0
tqkoh <shktmygs@gmail.com>
2023-03-19 00:27:08 +0900

    use db

---
+++ scripts/db.ts
@@ -0,0 +1,9 @@
+import * as mariadb from "mariadb";
+
+export const pool = mariadb.createPool({
+  host: process.env.MARIADB_HOSTNAME,
+  user: process.env.MARIADB_USERNAME,
+  password: process.env.MARIADB_PASSWORD,
+  database: process.env.MARIADB_DATABASE,
+  connectionLimit: 5,
+});
