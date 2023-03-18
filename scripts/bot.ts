c9b22667bf503ba106c5732170b836b1e757b60a
tqkoh <shktmygs@gmail.com>
2023-03-18 17:34:13 +0900

    impl

--- scripts/bot.ts
+++ scripts/bot.ts
@@ -1,18 +1,22 @@
 // traQのAPIを使いたい場合
 import { Apis, Configuration as TraQConfiguration } from "@traptitech/traq";
 import axios from "axios";
+import CronJob from "cron";
 import {
   ChatCompletionRequestMessage,
   OpenAIApi,
   Configuration as OpenAIConfiguration,
 } from "openai";
 
-const INTERVAL = 1 * 60 * 60 * 1000; // 1h
+const TYPING_INTERVAL = 3000;
+const MAX_REPLIES = 3;
 
 // ShowcaseにHUBOT_TRAQ_ACCESS_TOKENという名前で保存した環境変数を取得する
-const TQKSUB_CHANNEL_ID = "8c8172ca-8f7d-4204-b252-4e1e9b6f236b";
+const HOME_CHANNEL_ID = "8c8172ca-8f7d-4204-b252-4e1e9b6f236b";
+const TQK_NAME = "tqk";
+const TQK_DUMMY = "mastqk";
 const TOKEN = process.env.HUBOT_TRAQ_ACCESS_TOKEN;
-const PROMPT_URL = process.env.HUBOT_PROMPT_URL;
+const PROMPT_URL = process.env.PROMPT_URL;
 
 const traQConfiguration = new TraQConfiguration({
   accessToken: TOKEN,
@@ -31,62 +35,100 @@ interface PromptMessage {
 
 interface GetPromptResponse {
   version: string;
+  tweetQuestion: string;
   prompt: [PromptMessage];
 }
 
+function talk(mention: boolean, messageRes = null, plainText = "") {
+  axios
+    .get<GetPromptResponse>(PROMPT_URL, { withCredentials: true })
+    .then((res) => {
+      let messages: ChatCompletionRequestMessage[] = res.data.prompt.map(
+        (m) => {
+          const role =
+            m.role === "user"
+              ? "user"
+              : m.role === "system"
+              ? "system"
+              : "assistant";
+          return {
+            role: role,
+            content: m.content,
+          };
+        }
+      );
+
+      const now = new Date();
+      console.log(now.toLocaleTimeString());
+      const timeString = `${
+        ["日", "月", "火", "水", "木", "金", "土"][now.getDay()]
+      }曜日の ${String(now.getHours()).padStart(2, "0")}:${String(
+        now.getMinutes()
+      ).padStart(2, "0")}`;
+      // "日曜日の 00:00"
+      messages[messages.length - 1].content = messages[
+        messages.length - 1
+      ].content
+        .replace(/\{% time %\}/, timeString)
+        .replace(/\{% text %\}/, mention ? plainText : res.data.tweetQuestion);
+      console.log(messages.slice(-1)[0].content);
+
+      openai
+        .createChatCompletion({
+          model: "gpt-3.5-turbo",
+          messages: messages,
+          // max_tokens: 80,
+        })
+        .then((completion) => {
+          let replies = completion.data.choices[0].message.content
+            .split("\n")
+            .reduce((acc, c) => {
+              if (c !== "") {
+                acc.push(c.replace(TQK_DUMMY, TQK_NAME));
+              }
+              return acc;
+            }, []);
+
+          for (let i = 0; i < Math.min(replies.length, MAX_REPLIES); i++) {
+            setTimeout(() => {
+              console.log(replies[i]);
+              if (messageRes !== null) {
+                if (i === 0) {
+                  messageRes.reply(completion.data.choices[0].message.content);
+                } else {
+                  messageRes.send(completion.data.choices[0].message.content);
+                }
+              }
+            }, TYPING_INTERVAL * i);
+          }
+        });
+    });
+}
+
+// talk(false);
+
 module.exports = (robot) => {
   // 起動時
-  robot.send({ channelID: TQKSUB_CHANNEL_ID }, "ご");
+  robot.send({ channelID: HOME_CHANNEL_ID }, "ご");
+  console.log("aasd");
 
-  // メンション付き、または監視チャンネルで正規表現にマッチするメッセージが送られてきたときに反応
-  const talk = (message) => {
-    const { plainText, user, channelId, id } = message;
-    axios
-      .get<GetPromptResponse>(PROMPT_URL, { withCredentials: true })
-      .then((res) => {
-        const messages: ChatCompletionRequestMessage[] = res.data.prompt.map(
-          (m) => {
-            const role =
-              m.role === "user"
-                ? "user"
-                : m.role === "system"
-                ? "system"
-                : "assistant";
-            return {
-              role: role,
-              content: m.content,
-            };
-          }
-        );
-        openai
-          .createChatCompletion({
-            model: "gpt-3.5-turbo",
-            messages: messages,
-            temperature: 0.9,
-            max_tokens: 200,
-          })
-          .then((completion) => {
-            console.log(completion.data.choices[0].message);
-          });
-      });
-  };
+  new CronJob("*/0 0 */1 * * *", () => {
+    talk(false);
+  }).start();
+
   robot.respond(/.*/, (res) => {
     const { message } = res.message;
     const { plainText, user, channelId, id } = message;
-    if (/(おいす[ー～]?|join)$/i.test(plainText) || /leave$/i.test(plainText)) {
+    if (
+      /(おいす[ー～]?|\/?join)$/i.test(plainText) ||
+      /\/?leave$/i.test(plainText)
+    ) {
       return;
     }
-    talk(message);
-    console.log(message);
-    console.log(plainText); // 送信されてきたメッセージのプレーンテキスト
-    console.log(user.bot); // 送信者がbotの場合true
-    console.log(channelId); // メッセージが送信されたチャンネルのチャンネルID
-    console.log(id); // 送信されたメッセージのメッセージID
-    res.send("fuga");
-    if (user.id === "tqk") {
-      setInterval(() => {
-        res.send("bu2");
-      }, INTERVAL);
-    }
+    talk(
+      true,
+      res,
+      (user.name === TQK_NAME ? TQK_DUMMY : user.name) + ": " + plainText
+    );
   });
 };
--- scripts/cmd.ts
+++ scripts/cmd.ts
@@ -5,17 +5,20 @@ import {
   PostBotActionLeaveRequest,
   Configuration as TraQConfiguration,
 } from "@traptitech/traq";
+import { readFileSync } from "fs";
 
 const BOT_ID = process.env.HUBOT_TRAQ_BOT_ID;
 const TOKEN = process.env.HUBOT_TRAQ_ACCESS_TOKEN;
-const TQKSUB_CHANNEL_ID = "8c8172ca-8f7d-4204-b252-4e1e9b6f236b";
+const SUB_CHANNEL_ID = "8c8172ca-8f7d-4204-b252-4e1e9b6f236b";
 const traQConfiguration = new TraQConfiguration({
   accessToken: TOKEN,
 });
 const traq = new Apis(traQConfiguration); // api.hoge()でtraQのAPIが使える
 
+const readme = readFileSync("readme.md", "utf-8");
+
 module.exports = (robot) => {
-  robot.respond(/(おいす[ー～]?|join)$/i, async (res) => {
+  robot.respond(/(おいす[ー～]?|\/?join)$/i, async (res) => {
     const { message } = res.message;
     const { channelId, id } = message;
     try {
@@ -26,12 +29,12 @@ module.exports = (robot) => {
       res.reply("おいす");
     } catch (err) {
       robot.send(
-        { channelID: TQKSUB_CHANNEL_ID },
+        { channelID: SUB_CHANNEL_ID },
         `${err}\n//q.trap.jp/messages/${id}`
       );
     }
   });
-  robot.respond(/leave$/i, async (res) => {
+  robot.respond(/\/?leave$/i, async (res) => {
     const { message } = res.message;
     const { channelId, id } = message;
     try {
@@ -41,9 +44,15 @@ module.exports = (robot) => {
       res.send({ type: "stamp", name: "kan" }, { type: "stamp", name: "wave" });
     } catch (err) {
       robot.send(
-        { channelID: TQKSUB_CHANNEL_ID },
+        { channelID: SUB_CHANNEL_ID },
         `${err}\n//q.trap.jp/messages/${id}`
       );
     }
+  });
+  robot.respond(/(たすけて|help)$/i, async (res) => {
+    const { message } = res.message;
+    const { channelId, user, id } = message;
+    if (user.bot) return;
+    res.reply(readme);
   });
 };
