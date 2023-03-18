1c0234d45c658ecfb727acb08402d82e6dbdd2aa
tqkoh <shktmygs@gmail.com>
2023-03-18 23:55:00 +0900

    impl join leave

--- scripts/cmd.ts
+++ scripts/cmd.ts
@@ -1,15 +1,12 @@
 // traQのAPIを使いたい場合
-import {
-  Apis,
-  PostBotActionJoinRequest,
-  PostBotActionLeaveRequest,
-  Configuration as TraQConfiguration,
-} from "@traptitech/traq";
+import { Apis, Configuration as TraQConfiguration } from "@traptitech/traq";
+import axios from "axios";
 import { readFileSync } from "fs";
 
 const BOT_ID = process.env.HUBOT_TRAQ_BOT_ID;
 const TOKEN = process.env.HUBOT_TRAQ_ACCESS_TOKEN;
 const SUB_CHANNEL_ID = "8c8172ca-8f7d-4204-b252-4e1e9b6f236b";
+const CHANNELS_URL = process.env.CHANNELS_URL;
 const REPLY_DELAY = 500;
 const traQConfiguration = new TraQConfiguration({
   accessToken: TOKEN,
@@ -18,39 +15,45 @@ const traq = new Apis(traQConfiguration); // api.hoge()でtraQのAPIが使える
 
 const readme = readFileSync("readme.md", "utf-8");
 
+// axios
+//   .delete(
+//     CHANNELS_URL + "api/channels/" + "8c8172ca-8f7d-4204-b252-4e1e9b6f236b"
+//   )
+//   .then((_) => {
+//     console.log("kan");
+//   })
+//   .catch((e) => {
+//     console.log(e);
+//   });
+
 module.exports = (robot) => {
   robot.respond(/(おいす[ー～]?|\/?join)$/i, async (res) => {
     const { message } = res.message;
-    const { channelId, id } = message;
-    try {
-      await traq.letBotJoinChannel(BOT_ID, {
-        channelId,
-      } as PostBotActionJoinRequest);
+    const { channelId } = message;
+    if (!channelId) {
+      res.reply("チャンネルで使ってね");
+      return;
+    }
+
+    res.send({ type: "stamp", name: "loading" });
+    axios.post(CHANNELS_URL + "api/channels/" + channelId).then((_) => {
       res.send({ type: "stamp", name: "kan" });
       setTimeout(() => {
         res.reply("おいす");
       }, REPLY_DELAY);
-    } catch (err) {
-      robot.send(
-        { channelID: SUB_CHANNEL_ID },
-        `${err}\n//q.trap.jp/messages/${id}`
-      );
-    }
+    });
   });
   robot.respond(/\/?leave$/i, async (res) => {
     const { message } = res.message;
-    const { channelId, id } = message;
-    try {
-      await traq.letBotLeaveChannel(BOT_ID, {
-        channelId,
-      } as PostBotActionLeaveRequest);
-      res.send({ type: "stamp", name: "kan" }, { type: "stamp", name: "wave" });
-    } catch (err) {
-      robot.send(
-        { channelID: SUB_CHANNEL_ID },
-        `${err}\n//q.trap.jp/messages/${id}`
-      );
-    }
+    const { channelId } = message;
+
+    res.send({ type: "stamp", name: "loading" });
+    axios.delete(CHANNELS_URL + "api/channels/" + channelId).then((_) => {
+      res.send({ type: "stamp", name: "kan" });
+      setTimeout(() => {
+        res.reply(":wave:");
+      }, REPLY_DELAY);
+    });
   });
   robot.respond(/(たすけて|help)$/i, async (res) => {
     const { message } = res.message;
